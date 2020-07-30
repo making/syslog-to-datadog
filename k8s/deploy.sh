27433d5d32a35ccd59cb108bfd9cb7f6fa323490
Toshiaki Maki <tmaki@pivotal.io>
2020-07-30 16:33:46 +0900

    WIP

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
+ytt -f config -f values.yml -v pipeline_hash=$(shasum config/pipeline.yml | awk '{print $1}') > /tmp/deploy.yml
 kubectl create -n observability configmap logstash-pipeline --from-file=../pipeline -o yaml --dry-run > config/pipeline.yml
-kapp -a syslog-to-datadog deploy -c \
-     -f <(ytt -f config -f values.yml \
-     	      -v pipeline_hash=$(shasum config/pipeline.yml | awk '{print $1}')) $@
+kapp -a syslog-to-datadog deploy -c -f /tmp/deploy.yml $@
